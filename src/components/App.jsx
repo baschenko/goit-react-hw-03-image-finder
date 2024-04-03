b467803f9450c0b71648198893873ff2a04c82dd
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-04-04 02:15:46 +0300

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Container } from './App.styled';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ToastContainer } from 'react-toastify';
+import ImageGallery from './ImageGallery/ImageGallery';
+import 'react-toastify/dist/ReactToastify.css';
+
+export class App extends Component {
+  state = {
+    imgName: '',
+  };
+
+  onSubmitSearchbar = imgName => {
+    this.setState({ imgName });
+  };
+
+  render() {
+    return (
+      <Container>
+        <Searchbar onSubmit={this.onSubmitSearchbar} />
+        <ImageGallery imgInfo={this.state.imgName} />
+
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </Container>
+    );
+  }
+}
